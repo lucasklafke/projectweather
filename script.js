1f9dcf12cbcb459cddf6e8065b9d26f14c1ef7ff
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-03-04 19:15:04 -0300

    fix: obtençao do clima funcionando

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let lon = "35"; let lat = "139";
+let lon = "139"; let lat = "35";
 const APIKEY = "cf9af733cc2fc3efe6212cd3ca143c8f";
 
 function getLocalClimate(){
-    const promise = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKEY}`);
+    const promise = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${APIKEY}`);
     promise.then(showClimateInformation);
     promise.catch(treatError);
 }
@@ -12,7 +12,7 @@ function showClimateInformation(weatherResponse){
 }
 
 function treatError(error){
-    console.log(error);
+    console.log(error.reponse);
 }
 
 getLocalClimate()
