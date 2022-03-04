8fe3c1873472371d070856396f378e95d75921fb
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-03-04 20:04:16 -0300

    feat: imprime dados do clima na tela

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
-let lon = "139";
-let lat = "35";
-
+let lon;
+let lat;
 const APIKEY = "cf9af733cc2fc3efe6212cd3ca143c8f";
+let divClimateInformation = document.querySelector(".climate-information");
 
 function getLocalClimate() {
   const promise = axios.get(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${APIKEY}`
   );
-  promise.then(showClimateInformation);
+  promise.then(saveClimateInformation);
   promise.catch(treatError);
 }
 
-function showClimateInformation(weatherResponse) {
+function saveClimateInformation(weatherResponse) {
+  const local = weatherResponse.data.name;
+  const temperature = weatherResponse.data.main.temp;
+  const climate = weatherResponse.data.weather[0].main;
+
+  printClimateInformation(local,temperature,climate);
+
   console.log(weatherResponse);
+//   console.log(weatherResponse.data.name);
+//   console.log(weatherResponse.data.main.temp);
+//   console.log(weatherResponse.data.weather[0].main);
+}
+
+function printClimateInformation(local,temperature,climate){
+    divClimateInformation.innerHTML += `
+        <h4>${local}</h4>
+        <p>${temperature}</p>
+        <p>${climate}</p>
+    `;
 }
 
 function treatError(error) {
   console.log(error.response);
 }
-
-getLocalClimate();
 
 var x = document.getElementById("demo");
 function getLocation() {
@@ -30,11 +45,15 @@ function getLocation() {
   }
 }
 function showPosition(position) {
+  lat = position.coords.latitude;
+  lon = position.coords.longitude;
   x.innerHTML =
     "Latitude: " +
-    position.coords.latitude +
+    lat +
     "<br>Longitude: " +
-    position.coords.longitude;
+    lon;
+
+  getLocalClimate();
 }
 
 const sliderLon = document.getElementById("myRangeLon");
@@ -49,3 +68,7 @@ sliderLon.oninput = function () {
 const sliderLat = document.getElementById("myRangeLat");
 const outputLat = document.getElementById("demoLat");
 outputLat.innerHTML = sliderLat.value; // Display the default slider value
+
+sliderLat.oninput = function () {
+    outputLat.innerHTML = this.value;
+};
