db03b9ba2a628a0041d29bc0c3d6d971ebf78978
GuilhermeCRB <guilhermecrbarros@gmail.com>
2022-03-04 20:20:59 -0300

    feat: acrescentado icone do clima

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const APIKEY = "cf9af733cc2fc3efe6212cd3ca143c8f";
 let divClimateInformation = document.querySelector(".climate-information");
 
 function getLocalClimate() {
+  divClimateInformation.innerHTML = "";
   const promise = axios.get(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${APIKEY}`
   );
@@ -15,15 +16,17 @@ function saveClimateInformation(weatherResponse) {
   const local = weatherResponse.data.name;
   const temperature = weatherResponse.data.main.temp;
   const climate = weatherResponse.data.weather[0].main;
+  const icon = weatherResponse.data.weather[0].icon;
 
-  printClimateInformation(local,temperature,climate);
+  printClimateInformation(local,icon,temperature,climate);
 
   console.log(weatherResponse);
 }
 
-function printClimateInformation(local,temperature,climate){
+function printClimateInformation(local,icon,temperature,climate){
     divClimateInformation.innerHTML += `
         <h4>${local}</h4>
+        <img src="http://openweathermap.org/img/wn/${icon}@2x.png"/>
         <p>${temperature}</p>
         <p>${climate}</p>
     `;
